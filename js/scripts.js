7a3919147599410688b7359e4255a7716d828473
Gracie Lundell <gracielundell@gmail.com>
2016-05-12 11:47:42 -0700

    add blog posts

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,19 @@ $(document).ready(function() {
   $('a').each(function() {
     if($(this).prop('href') == window.location.href) {
       $(this).addClass('active');
-    }
+    };
   });
+  $("#angularVSember").click(function(){
+    $("#blogPost2").slideDown("slow");
+    $("#angularVSember").click(function() {
+         $("#blogPost2").slideUp("slow");
+    });
+  });
+  $("#htmlBasics").click(function(){
+    $("#blogPost1").slideDown("slow");
+    $("#htmlBasics").click(function() {
+         $("#blogPost1").slideUp("slow");
+    });
+  });
+
 });
